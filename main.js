09dde53823ec3e91568a5714535796ca34b644ed
nahidalam <shapla@gmail.com>
2017-06-21 15:01:37 -0700

    reset button action

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ function showHide(){
     hiddenitems[i].style.display="inline";
   }
 }
+
+function loadInitial(){
+  var hiddenitems = document.getElementsByClassName("hidden");
+  for (var i =0; i!=hiddenitems.length;i++){
+    hiddenitems[i].style.display="none";
+  }
+
+}
+
+function doLocal(){
+
+}
+function doDist(){
+
+}
+function doGT(){
+
+}
