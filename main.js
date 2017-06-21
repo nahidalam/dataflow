9c85a526dffcc73109f397a184bd7f58cfc48751
nahidalam <shapla@gmail.com>
2017-06-21 15:34:46 -0700

    saved local, removed reset error

--- main.js
+++ main.js
@@ -11,11 +11,22 @@ function loadInitial(){
   for (var i =0; i!=hiddenitems.length;i++){
     hiddenitems[i].style.display="none";
   }
+  var hiddentext = document.getElementsByClassName("hiddenLocalMsg");
+  for (var i =0; i!=hiddentext.length;i++){
+    hiddentext[i].style.display="none";
+  }
 
 }
 
 function doLocal(){
-
+  var hiddenitems = document.getElementsByClassName("hidden");
+  for (var i =0; i!=hiddenitems.length;i++){
+    hiddenitems[i].style.display="none";
+  }
+  var hiddentext = document.getElementsByClassName("hiddenLocalMsg");
+  for (var i =0; i!=hiddentext.length;i++){
+    hiddentext[i].style.display="inline";
+  }
 }
 function doDist(){
 
